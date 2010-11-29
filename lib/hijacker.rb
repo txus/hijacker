477349cb51014be025eb65c773a6fa2e9f4b9c5b
Josep M. Bach <josep.m.bach@gmail.com>
2010-11-29 21:34:56 +0100

    Extract spy methods from hijacker

--- lib/hijacker.rb
+++ lib/hijacker.rb
@@ -2,68 +2,19 @@ require 'drb'
 require 'trollop'
 require 'hijacker/exceptions'
 require 'hijacker/method_definer'
+require 'hijacker/spy'
 require 'hijacker/config'
 require 'hijacker/handler'
 
 module Hijacker
 
-  # Methods that won't be hijacked in any case
-  REJECTED_METHODS = (Object.instance_methods | Module.methods | %w{__original_[\w\d]+})
-  FORBIDDEN_CLASSES = [Array, Hash, String, Fixnum, Float, Numeric, Symbol, Proc, Class, Object, Module]
-
-  extend MethodDefiner # declares `define_hijacked`
-  private :define_hijacked
-
   class << self
 
-    def spying(*args, &block)
-      raise "No block given" unless block
-      Hijacker.spy(*args)
-      block.call
-      Hijacker.restore(args.first)
-    end
+    include MethodDefiner
+    private :define_hijacked
 
-    def spy(object, options = {})
-      raise "Cannot spy on the following forbidden classes: #{FORBIDDEN_CLASSES.map(&:to_s).join(', ')}" if FORBIDDEN_CLASSES.include?(object)
-      rejection = /^(#{REJECTED_METHODS.join('|')})/
-      only = options[:only]
-      uri = options[:uri]
-      custom_rejection = options[:reject] if options[:reject].is_a?(Regexp)
-       
-      inst_methods = guess_instance_methods_from(object).reject{|m| (m =~ rejection)}.reject{|m| m =~ custom_rejection}
-      sing_methods = guess_class_methods_from(object).reject{|m| m =~ rejection}.reject{|m| m =~ custom_rejection}
-
-      receiver = if object.is_a?(Class)
-        object
-      else
-        (class << object; self; end)
-      end
-
-      define_hijacked(inst_methods, receiver, uri) unless options[:only] == :singleton_methods
-      receiver = (class << object; self; end)
-      define_hijacked(sing_methods, receiver, uri) unless options[:only] == :instance_methods
-
-    end
-
-    def restore(object)
-      receiver = if object.is_a?(Class)
-        object
-      else
-        (class << object; self; end)
-      end
-      guess_instance_methods_from(object).select{|m| m =~ /__original_/}.each do |met|
-        met = met.to_s.gsub!("__original_", "")
-        receiver.send(:undef_method, :"#{met}")
-        receiver.send(:alias_method, :"#{met}", :"__original_#{met}")
-      end
-
-      receiver = (class << object; self; end)
-      guess_class_methods_from(object).select{|m| m =~ /__original_/}.each do |met|
-        met = met.to_s.gsub!("__original_", "")
-        receiver.send(:undef_method, :"#{met}")
-        receiver.send(:alias_method, :"#{met}", :"__original_#{met}")
-      end
-    end
+    include Spy
+    public :spy, :spying, :restore
 
     def register(method, args, retval, raised, object, uri = nil)
       args.map! do |arg|
@@ -78,24 +29,6 @@ module Hijacker
 
       server = DRbObject.new nil, (uri || self.drb_uri)
       server.handle method, args, retval, raised, object
-    end
-
-    private
-
-    def guess_instance_methods_from(object)
-      if object.is_a?(Class)
-        object.instance_methods
-      else
-        object.methods
-      end
-    end
-
-    def guess_class_methods_from(object)
-      if object.is_a?(Class)
-        object.methods
-      else
-        []
-      end
     end
 
   end
--- spec/hijacker_spec.rb
+++ spec/hijacker_spec.rb
@@ -15,128 +15,17 @@ class MyClass
     b
   end
 
-  def foo=(value)
+  def baz=(value)
     @value = value
   end
 
-  def nasty
-    raise "Buuh"
+  def method_that_raises
+    raise StandardError, "Something went wrong"
   end
 
-end
+end unless defined?(MyClass)
 
 describe Hijacker do
-
-  describe "#spying" do
-    it 'runs a block spying on a particular object' do
-      blk = lambda {
-        MyClass.foo
-      }
-      Hijacker.should_receive(:spy).with(MyClass).once.ordered
-      MyClass.should_receive(:foo).once.ordered
-      Hijacker.should_receive(:restore).with(MyClass).once.ordered
-
-      Hijacker.spying(MyClass, &blk)
-    end
-
-    it 'raises if no block given' do
-      expect {
-        Hijacker.spying(MyClass)
-      }.to raise_error("No block given")
-    end
-  end
-
-  describe "#spy - #restore" do
-
-    describe "hijacking a Class" do
-      describe "instance methods" do
-        before(:each) do
-          Hijacker.spy(MyClass, :only => :instance_methods)
-        end
-        it "registers method calls without arguments" do
-          Hijacker.should_receive(:register).with(:foo, [], 7, nil, kind_of(MyClass), nil).ordered
-          MyClass.new.foo.should == 7
-        end
-        it "registers method calls with arguments" do
-          Hijacker.should_receive(:register).with(:bar, [2, "string"], "string", nil, kind_of(MyClass), nil).ordered
-          MyClass.new.bar(2, "string").should == "string"
-        end
-        after(:each) do
-          Hijacker.restore(MyClass)
-        end
-      end
-      describe "class methods" do
-        before(:each) do
-          Hijacker.spy(MyClass)
-        end
-        it "registers method calls without arguments" do
-          Hijacker.should_receive(:register).with(:foo, [], 7, nil, kind_of(Class), nil).ordered
-          MyClass.foo.should == 7
-        end
-        it "registers method calls with arguments" do
-          Hijacker.should_receive(:register).with(:bar, [2, "string"], "string", nil, kind_of(Class), nil).ordered
-          MyClass.bar(2, "string").should == "string"
-        end
-        after(:each) do
-          Hijacker.restore(MyClass)
-        end
-      end
-      describe "forbidden classes (are not hijacked)" do
-        [Array, Hash, String, Fixnum, Float, Numeric, Symbol].each do |forbidden|
-          it "protects #{forbidden}" do
-            expect {
-              Hijacker.spy(forbidden)
-            }.to raise_error
-          end
-        end
-      end
-    end
-    describe "hijacking an object" do
-      describe "instance methods" do
-        let(:object) { MyClass.new }
-
-        before(:each) do
-          def object.my_method
-            8
-          end
-          def object.my_method_with_args(a,b)
-            b
-          end
-          Hijacker.spy(object)
-        end
-        it "registers method calls without arguments" do
-          Hijacker.should_receive(:register).with(:foo, [], 7, nil, kind_of(MyClass), nil).ordered
-          Hijacker.should_receive(:register).with(:my_method, [], 8, nil, kind_of(MyClass), nil).ordered
-
-          object.foo.should == 7
-          object.my_method.should == 8
-        end
-        it "registers method calls with arguments" do
-          Hijacker.should_receive(:register).with(:bar, [2, "string"], "string", nil, kind_of(MyClass), nil).ordered
-          Hijacker.should_receive(:register).with(:my_method_with_args, [2, "string"], "string", nil, kind_of(MyClass), nil).ordered
-
-          object.bar(2, "string").should == "string"
-          object.my_method_with_args(2, "string").should == "string"
-        end
-        it "works well with writers" do
-          Hijacker.should_receive(:register).with(:foo=, [2], 2, nil, kind_of(MyClass), nil).ordered
-          object.foo = 2
-        end
-        it "records exceptions" do
-          Hijacker.should_receive(:register).with(:nasty, [], nil, kind_of(RuntimeError), kind_of(MyClass), nil).ordered
-          object.nasty rescue nil
-        end
-        it "does not affect other instances of the object's class" do
-          Hijacker.should_not_receive(:register)
-          MyClass.new.foo.should == 7
-        end
-        after(:each) do
-          Hijacker.restore(object)
-        end
-      end
-    end
-
-  end
 
   describe "#register" do
     it 'sends the registered method call to the DRb server' do
@@ -166,6 +55,5 @@ describe Hijacker do
       end
     end
   end
-
 
 end
